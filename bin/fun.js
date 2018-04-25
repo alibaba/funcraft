b6c42404351aaf023b62678e96d0668fcec6194c
Vangie Du <duwan@live.com>
2018-04-25 22:52:08 +0800

    refine cli by commander (#25)

--- bin/fun.js
+++ bin/fun.js
@@ -2,51 +2,42 @@
 
 'use strict';
 
-const [subcommand, ...args] = process.argv.slice(2);
 
-const usage = `
-usage: fun [--version] [--help]
-           <command> [<args>]
-
-These are common Fun commands used in various situations:
-
-start a working area
-   config      Configure the fun
-   validate    Validate a fun template
-   deploy      Deploy a project to AliCloud
-   build       Build the dependencies
-   help        Print help information
-
-
-'fun help -a' and 'fun help -g' list available subcommands and some
-concept guides. See 'fun help <command>' or 'fun help <concept>'
-to read about a specific subcommand or concept.
-`;
-
-if (!subcommand) {
-  console.log(usage);
-  process.exit(0);
-}
-
-var handle = function (err) {
+const handle = function (err) {
   console.error(err.stack);
   process.exit(-1);
 };
 
+const program = require('commander');
 
-if (subcommand === 'config') {
-  require('../lib/commands/config')(...args).catch(handle);
-} else if (subcommand === 'validate') {
-  require('../lib/commands/validate')(...args).catch(handle);
-} else if (subcommand === 'deploy') {
-  require('../lib/commands/deploy')(...args).catch(handle);
-} else if (subcommand === 'build') {
-  require('../lib/commands/build')(...args).catch(handle);
-} else if (subcommand === '--version' || subcommand === '-v') {
-  console.log(require('../package.json').version);
-  process.exit(0);
-} else {
-  console.log('unsupported subcommand.');
-  console.log('type: fun help');
-  process.exit(-1);
-}
+program.version(require('../package.json').version, '-v, --version')
+  .description('The fun tool use template.yml to describe the API Gateway & Function Compute things, then publish it online.');
+
+program.command('config')
+  .description('Configure the fun')
+  .action(()=> {
+    require('../lib/commands/config')().catch(handle);
+  });
+
+program.command('validate')
+  .description('Validate a fun template')
+  .option('-t, --template [template]', 'path of fun template file. defaults to \'template.{yaml|yml}\'', 'template.{yaml|yml}')
+  .action((options)=>{
+    require('../lib/commands/validate')(options.template).catch(handle);
+  });
+
+program.command('deploy [stage]')
+  .description('Deploy a project to AliCloud')
+  .action((stage)=> {
+    require('../lib/commands/deploy')(stage).catch(handle);
+  });
+
+program.command('build')
+  .description('Build the dependencies')
+  .action(()=>{
+    require('../lib/commands/build')().catch(handle);
+  });
+
+program.parse(process.argv);
+
+if (!program.args.length) {program.help();}
