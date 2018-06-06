e29c91c1610647b8b365bf541ab6c5223fa91ba8
tanhe123 <xiayule148@gmail.com>
2018-06-06 23:53:29 +0800

    preserving files permission while zipping

--- test/zip.test.js
+++ test/zip.test.js
@@ -10,7 +10,9 @@ describe('zip', () => {
   it('should ok', async function () {
     var func = {
       codes: [
-        'file1.txt'
+        'file1.txt',
+        'file-with-exec-permission.sh',
+        'file-with-link'
       ]
     };
     var base64 = await zip.compress(func, path.join(__dirname, 'figures'), 'local');
