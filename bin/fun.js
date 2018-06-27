3115244176b5c3fdc0c7bd41579cbdf1d9af19ec
tanhe123 <xiayule148@gmail.com>
2018-06-27 15:07:09 +0800

    add node version check

--- bin/fun.js
+++ bin/fun.js
@@ -7,6 +7,13 @@ const handle = function (err) {
   process.exit(-1);
 };
 
+const semver = require('semver')
+
+if ( ! semver.satisfies(process.version, '>=8.0.0') ) {
+  console.error('Error: require nodejs version >= 8.0.0');
+  process.exit(1);
+}
+
 const program = require('commander');
 
 program.version(require('../package.json').version, '-v, --version')
