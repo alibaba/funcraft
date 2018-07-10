9d6753a1952a6b53e7cd6492741cb957533bf3a9
Vangie Du <yixian.dw@alibaba-inc.com>
2018-07-10 14:56:53 +0800

    add `npm run binary` and `npm run upload`

---
+++ script/binary.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+PKG_VERSION=`node -p "require('./package').version"`
+
+## package to binary
+pkg -t node8-linux-x64,node8-linux-x86,node8-macos-x64,node8-macos-x86,node8-win-x64,node8-win-x86  --out-path output .
+
+## rename and zip output files
+cd output
+for f in fun-*; 
+do 
+	filename=fun-v${PKG_VERSION}${f##*fun}
+	mv $f $filename
+	zip $filename.zip $filename
+	rm $filename
+done
