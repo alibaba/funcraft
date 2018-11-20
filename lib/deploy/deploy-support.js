muxiangqiu/fun

support rds and mns trigger

lib/deploy/deploy-support.js
@@ -14,6 +14,7 @@ const pkg = require('../../package.json');
 const zip = require('../zip');
 const debug = require('debug')('fun:deploy');
 const osLocale = require('os-locale');
+const MNSClient = require('@alicloud/mns');
 
 const promiseRetry = require('../retry');
 
@@ -52,6 +53,22 @@ const getOtsClient = async (instanceName) => {
     keepAliveMsecs: 1000, // default 1000
     timeout: profile.timeout * 1000 
   });
+};
+
+
+const getMnsClient = async (topicName, region) => {
+  const profile = await getProfile();
+
+  return new MNSClient(profile.accountId, {
+    region: region,
+    accessKeyId: profile.accessKeyId,
+    accessKeySecret: profile.accessKeySecret,
+    // optional & default
+    secure: false, // use https or http
+    internal: false, // use internal endpoint
+    vpc: false // use vpc endpoint
+  });
+
 };
 
 const getPopClient = async() => {
@@ -104,14 +121,14 @@ async function makeLogstoreIndex(projectName, logstoreName) {
         return ;
       } catch (ex) {
         if (ex.code !== 'IndexConfigNotExist') {
-          debug("error when getIndexConfig, projectName is %s, logstoreName is %s, error is: \n%O", projectName, logstoreName, ex)
+          debug('error when getIndexConfig, projectName is %s, logstoreName is %s, error is: \n%O', projectName, logstoreName, ex);
 
           throw ex;
         } 
       }
 
       // create default logstore index. index configuration is same with sls console.
-      debug("logstore index not exist, try to create a default index for project %s logstore %s", projectName, logstoreName);
+      debug('logstore index not exist, try to create a default index for project %s logstore %s', projectName, logstoreName);
       await sls.createIndex(projectName, logstoreName, {
         ttl: 10,
         line: {
@@ -120,9 +137,9 @@ async function makeLogstoreIndex(projectName, logstoreName) {
           token: [...', \'";=()[]{}?@&<>/:\n\t\r']
         }
       });
-      debug("create default index success for project %s logstore %s", projectName, logstoreName)
+      debug('create default index success for project %s logstore %s', projectName, logstoreName);
     } catch (ex) {
-      debug("error when createIndex, projectName is %s, logstoreName is %s, error is: \n%O", projectName, logstoreName, ex)
+      debug('error when createIndex, projectName is %s, logstoreName is %s, error is: \n%O', projectName, logstoreName, ex);
 
       console.log(red(`\t\t\tretry ${times} times`));
       retry(ex);
@@ -146,7 +163,7 @@ async function makeLogstore({
       await sls.getLogStore(projectName, logstoreName);    
     } catch (ex) {
       if (ex.code !== 'LogStoreNotExist') {
-        debug("error when getLogStore, projectName is %s, logstoreName is %s, error is: \n%O", projectName, logstoreName, ex)
+        debug('error when getLogStore, projectName is %s, logstoreName is %s, error is: \n%O', projectName, logstoreName, ex);
 
         console.log(red(`\t\tretry ${times} times`));
 
@@ -163,7 +180,7 @@ async function makeLogstore({
           shardCount
         });
       } catch (ex) {
-        debug("error when createLogStore, projectName is %s, logstoreName is %s, error is: \n%O", projectName, logstoreName, ex)
+        debug('error when createLogStore, projectName is %s, logstoreName is %s, error is: \n%O', projectName, logstoreName, ex);
         console.log(red(`\t\tretry ${times} times`));
         retry(ex);
       }
@@ -176,7 +193,7 @@ async function makeLogstore({
           shardCount
         });
       } catch (ex) {
-        debug("error when updateLogStore, projectName is %s, logstoreName is %s, error is: \n%O", projectName, logstoreName, ex);
+        debug('error when updateLogStore, projectName is %s, logstoreName is %s, error is: \n%O', projectName, logstoreName, ex);
 
         if (ex.code !== 'ParameterInvalid' && ex.message !== 'no parameter changed') {
           console.log(red(`\t\tretry ${times} times`));
@@ -202,7 +219,7 @@ async function makeSlsProject(projectName, description) {
         console.log(red(`Log Service '${projectName}' may create by others, you should use a unique project name.`));
         process.exit(-1);
       } else if (ex.code !== 'ProjectNotExist') {
-        debug("error when getProject, projectName is %s, error is: \n%O", projectName, ex);
+        debug('error when getProject, projectName is %s, error is: \n%O', projectName, ex);
 
         console.log(red(`\tretry ${times} times`));
         retry(ex);
@@ -223,12 +240,29 @@ async function makeSlsProject(projectName, description) {
           console.error(red(`error: sls project ${projectName} already exist, it may be in other region or created by other users.`));
           process.exit(-1);
         } else {
-          debug("error when createProject, projectName is %s, error is: \n%O", projectName, ex);
+          debug('error when createProject, projectName is %s, error is: \n%O', projectName, ex);
 
           console.log(red(`\tretry ${times} times`));
           retry(ex);
         }
       }    
+    }
+  });
+}
+
+async function makeMnsTopic(topicName, properties) {
+  var region = properties.Region;
+  const mnsClient = await getMnsClient(topicName, region);
+  delete properties.Region;
+
+  await promiseRetry(async (retry, times) => {
+    try {
+      let res;
+      res = await mnsClient.createTopic(topicName, properties);
+      console.log('mms create topic response status code = ', res.code);
+    } catch(ex) {
+      console.log(red(`\tretry ${times} times`));
+      retry(ex);
     }
   });
 }
@@ -250,7 +284,7 @@ async function makeService({serviceName,
       if (ex.code === 'AccessDenied') {
         throw ex;
       } else if (ex.code !== 'ServiceNotFound') {
-        debug("error when getService, serviceName is %s, error is: \n%O", serviceName, ex);
+        debug('error when getService, serviceName is %s, error is: \n%O', serviceName, ex);
 
         console.log(red(`\tretry ${times} times`));
         retry(ex);
@@ -288,7 +322,7 @@ async function makeService({serviceName,
         service = await fc.updateService(serviceName, options);
       }
     } catch (ex) {
-      debug("error when createService or updateService, serviceName is %s, options is %j, error is: \n%O", serviceName, options, ex);
+      debug('error when createService or updateService, serviceName is %s, options is %j, error is: \n%O', serviceName, options, ex);
 
       console.log(red(`\tretry ${times} times`));
       retry(ex);
@@ -388,7 +422,9 @@ const triggerTypeMapping = {
   'Timer': 'timer',
   'HTTP': 'http',
   'Log': 'log',
-  'OSS': 'oss'
+  'OSS': 'oss',
+  'RDS': 'rds',
+  'MNSTopic': 'mns_topic'
 };
 
 async function getSourceArn(triggerType, triggerProperties) {
@@ -396,6 +432,13 @@ async function getSourceArn(triggerType, triggerProperties) {
 
   if (triggerType === 'Log') {
     return `acs:log:${profile.defaultRegion}:${profile.accountId}:project/${triggerProperties.LogConfig.Project}`;
+  }else if (triggerType === 'RDS') {
+    return `acs:rds:${profile.defaultRegion}:${profile.accountId}:dbinstance/${triggerProperties.InstanceId}`;
+  }else if (triggerType === 'MNSTopic') {
+    if (triggerProperties.Region !== undefined){
+      return `acs:mns:${triggerProperties.Region}:${profile.accountId}:/topics/${triggerProperties.TopicName}`;
+    }
+    return `acs:mns:${profile.defaultRegion}:${profile.accountId}:/topics/${triggerProperties.TopicName}`;
   }
   
   return ;
@@ -433,9 +476,17 @@ function getTriggerConfig(triggerType, triggerProperties) {
       },
       Enable: triggerProperties.Enable || true
     };
-  } 
+  } else if (triggerType === 'RDS') {
+    return {
+      subscriptionObjects: triggerProperties.SubscriptionObjects,
+      retry: triggerProperties.Retry,
+      concurrency: triggerProperties.Concurrency,
+      eventFormat: triggerProperties.EventFormat
+    };
+  }else if (triggerType === 'MNSTopic') {
+    return {};
+  }
   console.error(`trigger type is ${triggerType} not supported.`);
-  
 }
 
 async function makeInvocationRole(serviceName, functionName, triggerType) {
@@ -483,6 +534,47 @@ async function makeInvocationRole(serviceName, functionName, triggerType) {
             'log:GetConsumerGroupCheckPoint'
           ],
           'Resource': '*',
+          'Effect': 'Allow'
+        }
+      ]
+    });
+
+    await attachPolicyToRole(policyName, invocationRoleName, 'Custom');
+    return invocationRole.Role;
+
+  }else if (triggerType === 'RDS' || triggerType === 'MNSTopic') {
+    const invocationRoleName = `FunCreateRole-${serviceName}-${functionName}`;
+    var tMap = {
+      'RDS': 'rds',
+      'MNSTopic': 'mns'
+    };
+    var principalService = util.format('%s.aliyuncs.com', tMap[triggerType]);
+
+    const invocationRole = await makeRole(invocationRoleName, true, 'Used for fc invocation', {
+      'Statement': [
+        {
+          'Action': 'sts:AssumeRole',
+          'Effect': 'Allow',
+          'Principal': {
+            'Service': [
+              principalService
+            ]
+          }
+        }
+      ],
+      'Version': '1'
+    });
+
+    const policyName = `FunCreatePolicy-${serviceName}-${functionName}`;
+
+    await makePolicy(policyName, {
+      'Version': '1',
+      'Statement': [
+        {
+          'Action': [
+            'fc:InvokeFunction'
+          ],
+          'Resource': `acs:fc:*:*:services/${serviceName}/functions/*`,
           'Effect': 'Allow'
         }
       ]
@@ -554,7 +646,7 @@ async function makeCustomDomain({domainName,
       customDomain = await fc.getCustomDomain(domainName);
     } catch (ex) {
       if (ex.code !== 'DomainNameNotFound') {
-        debug("error when getCustomDomain, domainName is %s, error is: \n%O", domainName, ex);
+        debug('error when getCustomDomain, domainName is %s, error is: \n%O', domainName, ex);
 
         console.log(red(`\tretry ${times} times`));
         retry(ex);
@@ -580,7 +672,7 @@ async function makeCustomDomain({domainName,
         customDomain = await fc.updateCustomDomain(domainName, options);
       }
     } catch (ex) {
-      debug("error when createCustomDomain or updateCustomDomain, domainName is %s, options is %j, error is: \n%O", domainName, options, ex);
+      debug('error when createCustomDomain or updateCustomDomain, domainName is %s, options is %j, error is: \n%O', domainName, options, ex);
 
       console.log(red(`\tretry ${times} times`));
       retry(ex);
@@ -617,7 +709,7 @@ async function makeGroup(group) {
       }
 
     } catch (ex) {
-      debug("error when makeGroup, error is: \n%O", ex);
+      debug('error when makeGroup, error is: \n%O', ex);
 
       console.log(red(`\tretry ${times} times`));
       retry(ex);
@@ -749,19 +841,19 @@ async function makeApi(group, {
         });
       }
 
-      debug("api params is %j", params)
+      debug('api params is %j', params);
 
       if (!api) {
-        debug("create api")
+        debug('create api');
         api = await ag.createApi(params);
       } else {
-        debug("modify api")
+        debug('modify api');
         await ag.modifyApi(Object.assign(params, {
           ApiId: api.ApiId
         }));
       }
       
-      debug("deploy api, params is GroupId %s, ApiId %s, StageName: %s", group.GroupId, api.ApiId, stageName);
+      debug('deploy api, params is GroupId %s, ApiId %s, StageName: %s', group.GroupId, api.ApiId, stageName);
 
       await ag.deployApi({
         GroupId: group.GroupId,
@@ -792,7 +884,7 @@ async function makeApi(group, {
         console.error(red(ex.message));
         process.exit(-1);
       } else {
-        debug("error when makeApi, error is: \n%O", ex);
+        debug('error when makeApi, error is: \n%O', ex);
 
         console.log(red(ex.message));
         console.log(red(`\tretry ${times} times`));
@@ -865,7 +957,7 @@ async function makeOtsInstance(instanceName, clusterType, description) {
         console.error(red(ex.message));
         process.exit(-1);
       } else {
-        debug("error when makeOtsInstance, error is: \n%O", ex)
+        debug('error when makeOtsInstance, error is: \n%O', ex);
 
         console.error(red(`retry ${times} times`));
         retry(ex);
@@ -919,7 +1011,7 @@ async function makeOtsTable({
         debug('ots table already exists, but could not be updated');
       }
     } catch (ex) {
-      debug("error when makeOtsTable, error is: \n%O", ex)
+      debug('error when makeOtsTable, error is: \n%O', ex);
 
       console.log(red(`retry ${times} times`));
       retry(ex);
@@ -931,5 +1023,5 @@ module.exports = {
   makeApi, makeApiTrigger, makeFunction,
   makeGroup, makeOtsTable, makeOtsInstance, 
   makeOtsTrigger, makeService, makeTrigger, makeSlsProject, 
-  makeLogstore, makeLogstoreIndex, makeCustomDomain
+  makeLogstore, makeLogstoreIndex, makeCustomDomain, makeMnsTopic
 };

alibaba/funcraft

lib/deploy/deploy-support.js
@@ -14,11 +14,14 @@ const pkg = require('../../package.json');
 const zip = require('../zip');
 const debug = require('debug')('fun:deploy');
 const osLocale = require('os-locale');
+const MNSClient = require('@alicloud/mns');
 
 const promiseRetry = require('../retry');
 
 let {
-  makeRole, attachPolicyToRole, makePolicy
+  makeRole,
+  attachPolicyToRole,
+  makePolicy
 } = require('../ram');
 
 const readFile = util.promisify(fs.readFile);
@@ -50,13 +53,29 @@ const getOtsClient = async (instanceName) => {
     instance: instanceName,
     region: profile.defaultRegion,
     keepAliveMsecs: 1000, // default 1000
-    timeout: profile.timeout * 1000 
+    timeout: profile.timeout * 1000
   });
 };
 
-const getPopClient = async() => {
+
+const getMnsClient = async (topicName, region) => {
   const profile = await getProfile();
-  
+
+  return new MNSClient(profile.accountId, {
+    region: region,
+    accessKeyId: profile.accessKeyId,
+    accessKeySecret: profile.accessKeySecret,
+    // optional & default
+    secure: false, // use https or http
+    internal: false, // use internal endpoint
+    vpc: false // use vpc endpoint
+  });
+
+};
+
+const getPopClient = async () => {
+  const profile = await getProfile();
+
   return new Pop({
     endpoint: `http://ots.${profile.defaultRegion}.aliyuncs.com`,
     apiVersion: '2016-06-20',
@@ -101,13 +120,13 @@ async function makeLogstoreIndex(projectName, logstoreName) {
     try {
       try {
         await sls.getIndexConfig(projectName, logstoreName);
-        return ;
+        return;
       } catch (ex) {
         if (ex.code !== 'IndexConfigNotExist') {
           debug('error when getIndexConfig, projectName is %s, logstoreName is %s, error is: \n%O', projectName, logstoreName, ex);
 
           throw ex;
-        } 
+        }
       }
 
       // create default logstore index. index configuration is same with sls console.
@@ -143,7 +162,7 @@ async function makeLogstore({
   let exists = true;
   await promiseRetry(async (retry, times) => {
     try {
-      await sls.getLogStore(projectName, logstoreName);    
+      await sls.getLogStore(projectName, logstoreName);
     } catch (ex) {
       if (ex.code !== 'LogStoreNotExist') {
         debug('error when getLogStore, projectName is %s, logstoreName is %s, error is: \n%O', projectName, logstoreName, ex);
@@ -151,7 +170,7 @@ async function makeLogstore({
         console.log(red(`\t\tretry ${times} times`));
 
         retry(ex);
-      } else {exists = false;}
+      } else { exists = false; }
     }
   });
 
@@ -197,7 +216,7 @@ async function makeSlsProject(projectName, description) {
   await promiseRetry(async (retry, times) => {
     try {
       await sls.getProject(projectName);
-    } catch(ex) {
+    } catch (ex) {
       if (ex.code === 'Unauthorized') {
         console.log(red(`Log Service '${projectName}' may create by others, you should use a unique project name.`));
         process.exit(-1);
@@ -206,7 +225,7 @@ async function makeSlsProject(projectName, description) {
 
         console.log(red(`\tretry ${times} times`));
         retry(ex);
-      } else {projectExist = false;}   
+      } else { projectExist = false; }
     }
 
     if (projectExist) {
@@ -217,7 +236,7 @@ async function makeSlsProject(projectName, description) {
       try {
         await sls.createProject(projectName, {
           description
-        });    
+        });
       } catch (ex) {
         if (ex.code === 'ProjectAlreadyExist') {
           console.error(red(`error: sls project ${projectName} already exist, it may be in other region or created by other users.`));
@@ -228,14 +247,35 @@ async function makeSlsProject(projectName, description) {
           console.log(red(`\tretry ${times} times`));
           retry(ex);
         }
-      }    
+      }
     }
   });
 }
 
-async function makeService({serviceName, 
-  role, 
-  description, 
+async function makeMnsTopic(topicName, properties) {
+  var region = properties.Region;
+  const mnsClient = await getMnsClient(topicName, region);
+  
+  // just for deepping copy
+  var params = JSON.parse(JSON.stringify(properties));
+  delete params.Region;
+
+  await promiseRetry(async (retry, times) => {
+    try {
+      let res;
+      res = await mnsClient.createTopic(topicName, params);
+      console.log('mms create topic response status code = ', res.code);
+    } catch (ex) {
+      console.log(red(`\tretry ${times} times`));
+      retry(ex);
+    }
+  });
+}
+
+async function makeService({
+  serviceName,
+  role,
+  description,
   internetAccess = true,
   logConfig,
   vpcConfig
@@ -259,8 +299,8 @@ async function makeService({serviceName,
   });
 
   const options = {
-    description, 
-    role, 
+    description,
+    role,
     logConfig: {
       project: logConfig.Project || '',
       logstore: logConfig.Logstore || ''
@@ -274,7 +314,7 @@ async function makeService({serviceName,
     });
   }
 
-  if ( vpcConfig ) {
+  if (vpcConfig) {
     Object.assign(options, {
       vpcConfig
     });
@@ -306,7 +346,7 @@ async function getFunCodeAsBase64(codeUri) {
   } else {
     codeUri = './';
   }
-  
+
   return await zip.file(codeUri);
 }
 
@@ -314,7 +354,7 @@ function extractOssCodeUri(ossUri) {
   const prefixLength = 'oss://'.length;
 
   const index = ossUri.indexOf('/', prefixLength);
-  
+
   return {
     ossBucketName: ossUri.substring(prefixLength, index),
     ossObjectName: ossUri.substring(index + 1)
@@ -388,7 +428,9 @@ const triggerTypeMapping = {
   'Timer': 'timer',
   'HTTP': 'http',
   'Log': 'log',
-  'OSS': 'oss'
+  'OSS': 'oss',
+  'RDS': 'rds',
+  'MNSTopic': 'mns_topic'
 };
 
 async function getSourceArn(triggerType, triggerProperties) {
@@ -396,9 +438,16 @@ async function getSourceArn(triggerType, triggerProperties) {
 
   if (triggerType === 'Log') {
     return `acs:log:${profile.defaultRegion}:${profile.accountId}:project/${triggerProperties.LogConfig.Project}`;
+  } else if (triggerType === 'RDS') {
+    return `acs:rds:${profile.defaultRegion}:${profile.accountId}:dbinstance/${triggerProperties.InstanceId}`;
+  } else if (triggerType === 'MNSTopic') {
+    if (triggerProperties.Region !== undefined) {
+      return `acs:mns:${triggerProperties.Region}:${profile.accountId}:/topics/${triggerProperties.TopicName}`;
+    }
+    return `acs:mns:${profile.defaultRegion}:${profile.accountId}:/topics/${triggerProperties.TopicName}`;
   }
-  
-  return ;
+
+  return;
 }
 
 function getTriggerConfig(triggerType, triggerProperties) {
@@ -433,9 +482,17 @@ function getTriggerConfig(triggerType, triggerProperties) {
       },
       Enable: triggerProperties.Enable || true
     };
-  } 
+  } else if (triggerType === 'RDS') {
+    return {
+      subscriptionObjects: triggerProperties.SubscriptionObjects,
+      retry: triggerProperties.Retry,
+      concurrency: triggerProperties.Concurrency,
+      eventFormat: triggerProperties.EventFormat
+    };
+  } else if (triggerType === 'MNSTopic') {
+    return {};
+  }
   console.error(`trigger type is ${triggerType} not supported.`);
-  
 }
 
 async function makeInvocationRole(serviceName, functionName, triggerType) {
@@ -443,17 +500,15 @@ async function makeInvocationRole(serviceName, functionName, triggerType) {
     const invocationRoleName = `AliyunFcGeneratedInvocationRole-${serviceName}-${functionName}`;
 
     const invocationRole = await makeRole(invocationRoleName, true, 'Used for fc invocation', {
-      'Statement': [
-        {
-          'Action': 'sts:AssumeRole',
-          'Effect': 'Allow',
-          'Principal': {
-            'Service': [
-              'log.aliyuncs.com'
-            ]
-          }
+      'Statement': [{
+        'Action': 'sts:AssumeRole',
+        'Effect': 'Allow',
+        'Principal': {
+          'Service': [
+            'log.aliyuncs.com'
+          ]
         }
-      ],
+      }],
       'Version': '1'
     });
 
@@ -461,31 +516,67 @@ async function makeInvocationRole(serviceName, functionName, triggerType) {
 
     await makePolicy(policyName, {
       'Version': '1',
-      'Statement': [
-        {
-          'Action': [
-            'fc:InvokeFunction'
-          ],
-          'Resource': `acs:fc:*:*:services/${serviceName}/functions/*`,
-          'Effect': 'Allow'
-        },
-        {
-          'Action': [
-            'log:Get*',
-            'log:List*',
-            'log:PostLogStoreLogs',
-            'log:CreateConsumerGroup',
-            'log:UpdateConsumerGroup',
-            'log:DeleteConsumerGroup',
-            'log:ListConsumerGroup',
-            'log:ConsumerGroupUpdateCheckPoint',
-            'log:ConsumerGroupHeartBeat',
-            'log:GetConsumerGroupCheckPoint'
-          ],
-          'Resource': '*',
-          'Effect': 'Allow'
-        }
+      'Statement': [{
+        'Action': [
+          'fc:InvokeFunction'
+        ],
+        'Resource': `acs:fc:*:*:services/${serviceName}/functions/*`,
+        'Effect': 'Allow'
+      },
+      {
+        'Action': [
+          'log:Get*',
+          'log:List*',
+          'log:PostLogStoreLogs',
+          'log:CreateConsumerGroup',
+          'log:UpdateConsumerGroup',
+          'log:DeleteConsumerGroup',
+          'log:ListConsumerGroup',
+          'log:ConsumerGroupUpdateCheckPoint',
+          'log:ConsumerGroupHeartBeat',
+          'log:GetConsumerGroupCheckPoint'
+        ],
+        'Resource': '*',
+        'Effect': 'Allow'
+      }
       ]
+    });
+
+    await attachPolicyToRole(policyName, invocationRoleName, 'Custom');
+    return invocationRole.Role;
+
+  } else if (triggerType === 'RDS' || triggerType === 'MNSTopic') {
+    const invocationRoleName = `FunCreateRole-${serviceName}-${functionName}`;
+    var tMap = {
+      'RDS': 'rds',
+      'MNSTopic': 'mns'
+    };
+    var principalService = util.format('%s.aliyuncs.com', tMap[triggerType]);
+
+    const invocationRole = await makeRole(invocationRoleName, true, 'Used for fc invocation', {
+      'Statement': [{
+        'Action': 'sts:AssumeRole',
+        'Effect': 'Allow',
+        'Principal': {
+          'Service': [
+            principalService
+          ]
+        }
+      }],
+      'Version': '1'
+    });
+
+    const policyName = `FunCreatePolicy-${serviceName}-${functionName}`;
+
+    await makePolicy(policyName, {
+      'Version': '1',
+      'Statement': [{
+        'Action': [
+          'fc:InvokeFunction'
+        ],
+        'Resource': `acs:fc:*:*:services/${serviceName}/functions/*`,
+        'Effect': 'Allow'
+      }]
     });
 
     await attachPolicyToRole(policyName, invocationRoleName, 'Custom');
@@ -493,7 +584,7 @@ async function makeInvocationRole(serviceName, functionName, triggerType) {
     return invocationRole.Role;
   }
 
-  return ;
+  return;
 }
 
 async function makeTrigger({
@@ -519,14 +610,14 @@ async function makeTrigger({
   };
 
   const invocationRole = await makeInvocationRole(serviceName, functionName, triggerType);
-  if ( invocationRole ) {
+  if (invocationRole) {
     Object.assign(params, {
       'invocationRole': invocationRole.Arn
     });
   }
 
   const sourceArn = await getSourceArn(triggerType, triggerProperties);
-  if ( sourceArn ) {
+  if (sourceArn) {
     Object.assign(params, {
       'sourceArn': sourceArn
     });
@@ -543,8 +634,9 @@ async function makeTrigger({
 }
 
 
-async function makeCustomDomain({domainName, 
-  protocol, 
+async function makeCustomDomain({
+  domainName,
+  protocol,
   routeConfig
 }) {
   const fc = await getFcClient();
@@ -563,10 +655,10 @@ async function makeCustomDomain({domainName,
   });
 
   const options = {
-    protocol, 
+    protocol,
   };
 
-  if ( routeConfig ) {
+  if (routeConfig) {
     Object.assign(options, {
       routeConfig
     });
@@ -595,7 +687,7 @@ async function makeGroup(group) {
 
   const groupName = group.name;
   const groupDescription = group.description;
-  
+
   var findGroup;
 
   await promiseRetry(async (retry, times) => {
@@ -603,12 +695,12 @@ async function makeGroup(group) {
       var groups = await ag.describeApiGroups({
         GroupName: groupName
       });
-    
+
       var list = groups.ApiGroupAttributes.ApiGroupAttribute;
       findGroup = list.find((item) => {
         return item.GroupName === groupName;
       });
-    
+
       if (!findGroup) {
         findGroup = await ag.createApiGroup({
           GroupName: groupName,
@@ -668,18 +760,18 @@ async function makeApi(group, {
         ApiName: apiName,
         GroupId: group.GroupId
       });
-      
+
       var apiSummarys = result.ApiSummarys && result.ApiSummarys.ApiSummary;
       var api;
 
       if (apiSummarys) {
         api = apiSummarys.find(summary => summary.ApiName === apiName);
       }
-    
+
       const requestParameters = parameters.map((item) => {
         return Object.assign(getDefaultRequestParameter(), item);
       });
-    
+
       const serviceParameters = requestParameters.map((item) => {
         return {
           ServiceParameterName: item.apiParameterName,
@@ -688,19 +780,19 @@ async function makeApi(group, {
           ParameterCatalog: 'REQUEST'
         };
       });
-    
+
       const serviceParametersMap = requestParameters.map((item) => {
         return {
           ServiceParameterName: item.apiParameterName,
           RequestParameterName: item.apiParameterName
         };
       });
-    
+
       const mergedRequestConfig = Object.assign(getDefaultRequestConfig(method), {
         'RequestHttpMethod': method,
         'RequestPath': requestPath
       }, requestConfig);
-    
+
       const profile = await getProfile();
       var params = {
         GroupId: group.GroupId,
@@ -730,7 +822,7 @@ async function makeApi(group, {
             RoleArn: roleArn
           }
         }),
-        ResultType: resultConfig.resultType || 'passthrough', 
+        ResultType: resultConfig.resultType || 'passthrough',
         ResultSample: resultConfig.resultSample || 'result sample',
         FailResultSample: resultConfig.failResultSample || 'failed samples'
       };
@@ -760,7 +852,7 @@ async function makeApi(group, {
           ApiId: api.ApiId
         }));
       }
-      
+
       debug('deploy api, params is GroupId %s, ApiId %s, StageName: %s', group.GroupId, api.ApiId, stageName);
 
       await ag.deployApi({
@@ -769,12 +861,12 @@ async function makeApi(group, {
         StageName: stageName,
         Description: `deployed by fun at ${new Date().toISOString()}`
       });
-    
+
       const apiDetail = await ag.describeApi({
         GroupId: group.GroupId,
         ApiId: api.ApiId
       });
-    
+
       console.log('    URL: %s %s://%s%s',
         green(apiDetail.RequestConfig.RequestHttpMethod),
         apiDetail.RequestConfig.RequestProtocol.toLowerCase(),
@@ -797,7 +889,8 @@ async function makeApi(group, {
         console.log(red(ex.message));
         console.log(red(`\tretry ${times} times`));
         retry(ex);
-      }scrollTo;
+      }
+      scrollTo;
     }
   });
 }
@@ -843,7 +936,7 @@ async function makeOtsInstance(instanceName, clusterType, description) {
           'InstanceName': instanceName
         });
 
-        return ;
+        return;
       } catch (ex) {
         if (ex.code === 'AuthFailed') {
           console.log(red(ex.message));
@@ -852,7 +945,7 @@ async function makeOtsInstance(instanceName, clusterType, description) {
           throw ex;
         }
       }
-    
+
       await pop.request('InsertInstance', {
         InstanceName: instanceName,
         ClusterType: clusterType,
@@ -880,9 +973,10 @@ async function makeOtsTrigger({
   triggerName,
   stream
 }) {
+
   const [, , , , path] = stream.split(':');
   const [, instance, , table] = path.split('/');
-  
+
   // todo: implement
   console.error(`Try to create OTS Trigger of /instance/${instance}/table/${table}, but the SDK didn't OK.`);
 }
@@ -927,8 +1021,18 @@ async function makeOtsTable({
 }
 
 module.exports = {
-  makeApi, makeApiTrigger, makeFunction,
-  makeGroup, makeOtsTable, makeOtsInstance, 
-  makeOtsTrigger, makeService, makeTrigger, makeSlsProject, 
-  makeLogstore, makeLogstoreIndex, makeCustomDomain
+  makeApi,
+  makeApiTrigger,
+  makeFunction,
+  makeGroup,
+  makeOtsTable,
+  makeOtsInstance,
+  makeOtsTrigger,
+  makeService,
+  makeTrigger,
+  makeSlsProject,
+  makeLogstore,
+  makeLogstoreIndex,
+  makeCustomDomain,
+  makeMnsTopic
 };