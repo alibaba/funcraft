0db9fc46a0c84af586a54f23811e5900b760d604
tanhe123 <xiayule148@gmail.com>
2019-01-28 16:28:19 +0800

    update apigateway demo

--- examples/api-gateway/index.js
+++ examples/api-gateway/index.js
@@ -6,10 +6,10 @@ module.exports.handler = function (event, context, callback) {
 
     console.log("request: " + JSON.stringify(event.toString()));
 
-    //将 event 转化为 JSON 对象
+    // 将 event 转化为 JSON 对象
     event = JSON.parse(event.toString());
 
-    //根据用户输入的 statusCode 返回，可用于测试不同 statusCode 的情况
+    // 根据用户输入的 statusCode 返回，可用于测试不同 statusCode 的情况
     if (event.queryParameters !== null && event.queryParameters !== undefined) {
         if (event.queryParameters.httpStatus !== undefined && event.queryParameters.httpStatus !== null && event.queryParameters.httpStatus !== "") {
             console.log("Received http status: " + event.queryParameters.httpStatus);
@@ -17,23 +17,23 @@ module.exports.handler = function (event, context, callback) {
         }
     }
 
-    //如果 body 是 Base64 编码的，FC 中需要对 body 内容进行解码
+    // 如果 body 是 Base64 编码的，FC 中需要对 body 内容进行解码
     if (event.body !== null && event.body !== undefined) {
         if (event.isBase64Encoded !== null && event.isBase64Encoded !== undefined && event.isBase64Encoded) {
             event.body = new Buffer(event.body, 'base64').toString();
         }
     }
 
-    //input 是A PI网关给 FC 的输入内容
+    // input 是 API 网关给 FC 的输入内容
     var responseBody = {
         message: "Hello World!",
         input: event
     };
 
-    //对 body 内容进行 Base64 编码，可根据需要处理
+    // 对 body 内容进行 Base64 编码，可根据需要处理
     var base64EncodeStr = new Buffer(JSON.stringify(responseBody)).toString('base64');
 
-    //FC 给 API 网关返回的格式，须如下所示。isBase64Encoded 根据 body 是否 Base64 编码情况设置
+    // FC 给 API 网关返回的格式，须如下所示。isBase64Encoded 根据 body 是否 Base64 编码情况设置
     var response = {
         isBase64Encoded: true,
         statusCode: responseCode,
