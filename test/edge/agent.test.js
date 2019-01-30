f5682d9642a8245c8a36f7a03bc82c5cc566dc53
Feilong Wang <feilong.wfl@alibaba-inc.com>
2019-01-30 15:36:32 +0800

    add tests for edge subcommand

---
+++ test/edge/agent.test.js
@@ -0,0 +1,431 @@
+'use strict';
+
+const expect = require('expect.js');
+const sinon = require('sinon');
+
+const https = require('https');
+const EventEmitter = require('events');
+const agent = require('../../lib/edge/agent');
+
+const TEST_DEPLOY_FUNCTION = {
+  functionId: 'function_id',
+  serviceName: 'service_name',
+  functionName: 'function_name',
+  handler: 'index.handler',
+  runtime: 'nodejs8',
+  timeout: 3,
+  memory: 128,
+  codeDir: '/tmp/',
+  accountId: 'account_id',
+  region: 'cn-hangzhou',
+  pinned: true,
+  envVars: {
+    TEST_ENVIRONMENT: 'test_environment',
+  },
+  debugPort: 5700,
+};
+
+const TEST_INVOKE_FUNCTION = {
+  functionId: 'function_id',
+  serviceName: 'service_name',
+  functionName: 'function_name',
+  accountId: 'account_id',
+  region: 'cn-hangzhou',
+  event: '{}',
+};
+
+class ClientRequest extends EventEmitter {
+  write() {}
+  end() {}
+}
+
+const cookie = 'd3f19b4c9218f4fac6ead23554fd8aa4ba0d08a3';
+class ServerResponse extends EventEmitter {
+  constructor() {
+    super();
+    this.headers = {
+      'set-cookie': cookie,
+    };
+  }
+}
+
+describe('edge/FunctionComputeClient', function () {
+  let httpsRequest;
+  beforeEach(function () {
+    httpsRequest = sinon.stub(https, 'request');
+  });
+  afterEach(function () {
+    httpsRequest.restore();
+  });
+  describe('#login', function () {
+    it('should throw since request writing error', async function () {
+      const clientRequest = new ClientRequest();
+      const write = sinon.stub(clientRequest, 'write').callsFake(function () {
+        clientRequest.emit('error', new Error('Cannot write'));
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      let error;
+      try {
+        await client.login();
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      write.restore();
+    });
+
+    it('should throw since server internal error', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', '');
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      let error;
+      try {
+        await client.login();
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      end.restore();
+    });
+    it('should throw since authentication fails', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', JSON.stringify({
+          code: 400,
+          message: 'User name or password incorrect'
+        }));
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      let error;
+      try {
+        await client.login();
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      end.restore();
+    });
+    it('should pass since all requirements meet', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', JSON.stringify({
+          code: 200,
+        }));
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      let error;
+      try {
+        await client.login();
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.not.be.an(Error);
+      end.restore();
+
+    });
+  });
+  describe('#deploy', function () {
+    it('should throw since neither Function id nor serviceName/functionName is provided', async function () {
+      const info = Object({}, TEST_DEPLOY_FUNCTION, {
+        functionId: undefined,
+        serviceName: undefined,
+        functionName: undefined,
+      });
+      const client = new agent.FunctionComputeClient();
+      let error;
+      try {
+        await client.deploy(info);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+    });
+    it('should throw since login fails', async function () {
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').rejects(new Error('Cannot login'));
+      let error;
+      try {
+        await client.deploy(TEST_DEPLOY_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      login.restore();
+    });
+    it('should throw since request writing error', async function () {
+      const clientRequest = new ClientRequest();
+      const write = sinon.stub(clientRequest, 'write').callsFake(function () {
+        clientRequest.emit('error', new Error('Cannot write'));
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').resolves(cookie);
+      let error;
+      try {
+        await client.deploy(TEST_DEPLOY_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      login.restore();
+      write.restore();
+    });
+    it('should throw since server internal error', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', '');
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').resolves(cookie);
+      let error;
+      try {
+        await client.deploy(TEST_DEPLOY_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      login.restore();
+      end.restore();
+    });
+    it('should throw since memory size error', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', JSON.stringify({
+          code: 400,
+          message: 'Memory size is not a number'
+        }));
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').resolves(cookie);
+      const info = Object.assign({}, TEST_DEPLOY_FUNCTION, {
+        memory: '128',
+      });
+      let error;
+      try {
+        await client.deploy(info);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      login.restore();
+      end.restore();
+    });
+    it('should pass since all requirements meet', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', JSON.stringify({
+          code: 200,
+        }));
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').resolves(cookie);
+      let error;
+      try {
+        await client.deploy(TEST_DEPLOY_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.not.be.an(Error);
+      login.restore();
+      end.restore();
+    });
+  });
+  describe('#invoke', function () {
+    it('should throw since neither Function id nor serviceName/functionName is provided', async function () {
+      const info = {
+        accountId: TEST_DEPLOY_FUNCTION.accountId,
+        region: TEST_DEPLOY_FUNCTION.region,
+      };
+      const client = new agent.FunctionComputeClient();
+      let error;
+      try {
+        await client.invoke(info);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+    });
+    it('should throw since invocation type is neither Sync nor Async', async function () {
+      const info = Object.assign({}, TEST_INVOKE_FUNCTION, {
+        invocationType: 'Test'
+      });
+      const client = new agent.FunctionComputeClient();
+      let error;
+      try {
+        await client.invoke(info);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+    });
+    it('should throw since login fails', async function () {
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').rejects(new Error('Cannot login'));
+      let error;
+      try {
+        await client.invoke(TEST_INVOKE_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      login.restore();
+    });
+    it('should throw since request writing error', async function () {
+      const clientRequest = new ClientRequest();
+      const write = sinon.stub(clientRequest, 'write').callsFake(function () {
+        clientRequest.emit('error', new Error('Cannot write'));
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').resolves(cookie);
+      let error;
+      try {
+        await client.invoke(TEST_INVOKE_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      login.restore();
+      write.restore();
+    });
+    it('should throw since server internal error', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', '');
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').resolves(cookie);
+      let error;
+      try {
+        await client.invoke(TEST_INVOKE_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      login.restore();
+      end.restore();
+    });
+    it('should throw since function not found error', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', JSON.stringify({
+          code: 400,
+          message: 'Function not found'
+        }));
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').resolves(cookie);
+      let error;
+      try {
+        await client.invoke(TEST_INVOKE_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      login.restore();
+      end.restore();
+    });
+    it('should pass since all requirements meet', async function () {
+      let cb;
+      const serverResponse = new ServerResponse();
+      const clientRequest = new ClientRequest();
+      const end = sinon.stub(clientRequest, 'end').callsFake(function () {
+        cb(serverResponse);
+        serverResponse.emit('data', JSON.stringify({
+          code: 200,
+          data: Buffer.from('result').toString('base64'),
+        }));
+        serverResponse.emit('end');
+      });
+      httpsRequest.callsFake(function (options, callback) {
+        cb = callback;
+        return clientRequest;
+      });
+      const client = new agent.FunctionComputeClient();
+      const login = sinon.stub(client, 'login').resolves(cookie);
+      let error;
+      try {
+        await client.invoke(TEST_INVOKE_FUNCTION);
+      } catch (err) {
+        error = err;
+      }
+      console.log(error);
+      expect(error).to.not.be.an(Error);
+      login.restore();
+      end.restore();
+    });
+  });
+});
---
+++ test/edge/runtime.test.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const expect = require('expect.js');
+const sinon = require('sinon');
+const path = require('path');
+const util = require('util');
+
+const LocalRuntime = require('../../lib/edge/runtime');
+
+describe('edge/LocalRuntime', function () {
+  describe('#invoke', function () {
+    let config;
+    beforeEach(function () {
+      config = {
+        runtime: 'nodejs8',
+        identifier: {
+          region: 'ch-hangzhou',
+          accountId: 'account_id',
+          serviceName: 'service_name',
+          functionName: 'function_name',
+        },
+        handler: 'index.handler',
+        timeout: 3,
+        memory: 128,
+        codeAbsPath: path.resolve(__dirname, '../../examples/helloworld/helloworld.js'),
+      };
+    });
+
+    afterEach(function () {
+      config = undefined;
+    });
+    it('should fail since unsupported runtime', async function () {
+      config.runtime = 'java8';
+      const runtime = new LocalRuntime();
+      let error;
+      try {
+        await runtime.invoke(config, {});
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+    });
+    it('should fail since copying code to container fails', async function () {
+      const runtime = new LocalRuntime();
+      const stub = sinon.stub(runtime.container, 'copy').rejects(new Error('Cannot copy'));
+      let error;
+      try {
+        await runtime.invoke(config, {});
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      stub.restore();
+    });
+    it('should fail since deploying function fails', async function () {
+      const runtime = new LocalRuntime();
+      const copy = sinon.stub(runtime.container, 'copy').resolves();
+      const deploy = sinon.stub(runtime.client, 'deploy').rejects(new Error('Cannot deploy'));
+      let error;
+      try {
+        await runtime.invoke(config, {});
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      deploy.restore();
+      copy.restore();
+    });
+    it('should fail since invoking function fails', async function () {
+      const runtime = new LocalRuntime();
+      const copy = sinon.stub(runtime.container, 'copy').resolves();
+      const deploy = sinon.stub(runtime.client, 'deploy').resolves();
+      const invoke = sinon.stub(runtime.client, 'invoke').rejects(new Error('Cannot invoke'));
+      let error;
+      try {
+        await runtime.invoke(config, {});
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be.an(Error);
+      invoke.restore();
+      deploy.restore();
+      copy.restore();
+    });
+    it('should pass since all requirements meet', async function () {
+      const runtime = new LocalRuntime();
+      const copy = sinon.stub(runtime.container, 'copy').resolves();
+      const deploy = sinon.stub(runtime.client, 'deploy').resolves();
+      const invoke = sinon.stub(runtime.client, 'invoke').resolves(Buffer.from(''));
+      let error;
+      try {
+        await runtime.invoke(config, {});
+      } catch (err) {
+        error = err;
+      }
+      expect(error).to.be(undefined);
+      invoke.restore();
+      deploy.restore();
+      copy.restore();
+    });
+    it('should output debugger configs since output-debugger-configs is set', async function () {
+      const runtime = new LocalRuntime();
+      const copy = sinon.stub(runtime.container, 'copy').resolves();
+      const deploy = sinon.stub(runtime.client, 'deploy').resolves();
+      const invoke = sinon.stub(runtime.client, 'invoke').resolves(Buffer.from(''));
+      const spy = sinon.spy(console, 'log');
+      await runtime.invoke(config, {}, {
+        debugPort: 5700,
+        outputDebuggerConfigs: true,
+      });
+      expect(spy.calledWith(sinon.match('Attach to Fun (Node.js 8)')));
+      spy.restore();
+      invoke.restore();
+      deploy.restore();
+      copy.restore();
+    });
+    it('should output python debugger configs since runtime is python3', async function () {
+      config.runtime = 'python3';
+      const runtime = new LocalRuntime();
+      const copy = sinon.stub(runtime.container, 'copy').resolves();
+      const deploy = sinon.stub(runtime.client, 'deploy').resolves();
+      const invoke = sinon.stub(runtime.client, 'invoke').resolves(Buffer.from(''));
+      const spy = sinon.spy(console, 'log');
+      await runtime.invoke(config, {}, {
+        debugPort: 5700,
+        outputDebuggerConfigs: true,
+      });
+      expect(spy.calledWith(sinon.match('Attach to Fun (Python 3)')));
+      spy.restore();
+      invoke.restore();
+      deploy.restore();
+      copy.restore();
+    });
+  });
+});
