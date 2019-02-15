e187bd2138f2cea36e065c9d7d242a1490d6f2fd
Feilong Wang <feilong.wfl@alibaba-inc.com>
2019-02-15 20:29:54 +0800

    fix travis ci check failure

--- test/edge/runtime.test.js
+++ test/edge/runtime.test.js
@@ -3,7 +3,6 @@
 const expect = require('expect.js');
 const sinon = require('sinon');
 const path = require('path');
-const util = require('util');
 
 const LocalRuntime = require('../../lib/edge/runtime');
 
