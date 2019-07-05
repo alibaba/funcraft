7b86624eb3a627c7f7dade70395041922cfddd01
zqf <zqf@IT-FVFYH2N0J1WT.local>
2019-07-05 10:31:19 +0800

    qianfeng/add_nas_browser_cp_large_file

--- examples/nas_browser/nas.sh
+++ examples/nas_browser/nas.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-SHELL_DIR="$(dirname $0)"
+SHELL_DIR="$(dirname $0)" 
+
+chunkSize='95k'
+splitNameLen=10
 
 usage() {
     echo "usage:"
@@ -14,17 +17,17 @@ usage() {
     echo -e "\t./nas.sh bash <cmd>"
 }
 
-ENV_FILE=$(pwd)/.env
+ENV_FILE=$(pwd)/.env 
 
-if [ -f $ENV_FILE ]; then
+if [ -f $ENV_FILE ]; then 
     . $ENV_FILE
 fi
 
 FC_URL=${FC_URL:-http://localhost:8000/2016-08-15/proxy/nasDemo/browser/}
 
 checkArgs() {
-    if [ -z "$1" ]; then
-        usage
+    if [ -z "$1" ]; then 
+        usage 
         exit -1
     fi
 }
@@ -32,45 +35,105 @@ checkArgs() {
 cp() {
     checkArgs $1
     checkArgs $2
-    
+
     src=$1
     dst=$2
 
-
-    if [[ "$dst" =~ ^'fc:/' ]]; then
-        dstFile=${dst#fc:}
+    if [[ "$dst" =~ ^'fc:/' ]]; then 
+        dstFile=${dst#fc:} 
     else
-        echo "dst must start with fc:/ prefix";
+        echo "dst must start with fc:/ prefix"
         exit 1
     fi
 
     if [[ "$src" =~ ^'oss:' ]]; then
         ossFile=$src
 
-        curl -G -XPOST \
-            $FC_URL"cp" \
-            --data-urlencode "dst=$dstFile" \
-            --data-urlencode "oss=$ossFile"
-
+        curl -G -XPOST \                   
+        $FC_URL"cp" \                      
+        --data-urlencode "dst=$dstFile" \  
+        --data-urlencode "oss=$ossFile"    
         exit 0
     fi
 
     if [[ "$src" =~ ^'/' ]]; then
         absSrcFile=$src
     elif [[ "$src" =~ ^'~' ]]; then
-        absSrcFile=$HOME${dst#~}
+        absSrcFile=$HOME${src#~} 
     else
         currentDir=$(pwd)
         absSrcFile=$currentDir/$src
     fi
 
-    if [ ! -f "$absSrcFile" ]; then 
+    if [ ! -f "$absSrcFile" ]; then
         echo "file $absSrcFile not found."
     fi
+    
+    if [ -f "$absSrcFile" ]; then
+        srcFile=absSrcFile
+        absSrcFileHash=$(md5sum ${absSrcFile} | awk '{ print $1 }')
+    fi
 
-    curl -XPOST \
-        $FC_URL"cp?dst=$dstFile" \
-        -F "file=@$absSrcFile"
+    filename="$(basename "$absSrcFile")" 
+
+    cp_http_response=$(curl --silent --write-out "cp_http_status:%{http_code}" -G -XPOST \
+        $FC_URL"cp" \
+        --data-urlencode "file_hash=$absSrcFileHash" \
+        --data-urlencode "dst=$dstFile" \
+        --data-urlencode "dst_file_name=$filename")
+
+    cp_http_body=$(echo $cp_http_response | sed -e 's/cp_http_status\:.*//g')
+    cp_http_status=$(echo $cp_http_response | tr -d '\n' | sed -e 's/.*cp_http_status://')
+    echo "$cp_http_body"
+
+    if [ "$cp_http_status" -eq "200" ]; then
+        curDir="$(dirname "$absSrcFile")" 
+        tmpDir="$curDir/$filename"        
+        tmpDir+="-split"
+
+        if [ ! -d "$tmpDir" ]; then
+            mkdir $tmpDir
+        fi
+
+        split -b $chunkSize -a $splitNameLen $absSrcFile "$tmpDir/$filename-" 
+
+        declare -a splitFileArr
+        declare -a fileHashArr
+        len=0
+        for entry in "$tmpDir"/*; do
+            splitFileArr[${len}]=$entry
+            
+            fileHashArr[${len}]=$(md5sum ${entry} | awk '{ print $1 }')
+            ((len++))
+        done
+
+        seq=0
+        upload_fail=0
+        for sf in "${splitFileArr[@]}"; do
+            upload_http_response=$(curl  --silent --write-out "upload_http_status:%{http_code}" -XPOST \
+                $FC_URL"upload?file_hash=${fileHashArr[$seq]}&chunk_id=$seq&chunk_sum=$len&dst=$dstFile&task_id=$absSrcFileHash" \
+                -F "file=@$sf") 
+            
+            upload_http_body=$(echo $upload_http_response | sed -e 's/upload_http_status\:.*//g')
+            upload_http_status=$(echo $upload_http_response | tr -d '\n' | sed -e 's/.*upload_http_status://')
+            echo $upload_http_body
+            
+            if [ $upload_http_status -ne 200 ]; then
+                upload_fail=1
+                break
+            fi
+
+            ((seq++))
+        done
+        if [ $upload_fail -eq 0 ]; then
+            curl -G -XPOST \
+                $FC_URL"join" \
+                --data-urlencode "task_id=$absSrcFileHash" \
+                --data-urlencode "dst=$dstFile" \
+                --data-urlencode "dst_file_name=$filename"
+        fi
+        rm -r $tmpDir
+    fi
 }
 
 ls() {
@@ -83,7 +146,7 @@ ls() {
             $FC_URL"ls" \
             --data-urlencode "p=$dstDir"
     else
-        echo "file must start with fc:/ prefix";
+        echo "file must start with fc:/ prefix"
         exit 1
     fi
 }
@@ -98,13 +161,13 @@ cat() {
             $FC_URL"cat" \
             --data-urlencode "file=$file"
     else
-        echo "file must start with fc:/ prefix";
+        echo "file must start with fc:/ prefix"
         exit 1
     fi
 }
 
 bash() {
-    cmd=$(echo "$*")
+    cmd=$(echo "$*") # $* 以一个单字符串显示所有向脚本传递的参数。
     checkArgs $cmd
 
     curl -G -XPOST \
@@ -115,7 +178,7 @@ bash() {
 unzip() {
     checkArgs $1
     zipFile=$1
-    
+
     if [[ "$zipFile" =~ ^'fc:/' ]]; then
         zipFile=${zipFile#fc:}
 
@@ -123,22 +186,27 @@ unzip() {
             $FC_URL"unzip" \
             --data-urlencode "file=$zipFile"
     else
-        echo "file must start with fc:/ prefix";
+        echo "file must start with fc:/ prefix"
         exit 1
     fi
 }
 
 case "$1" in
-    cp ) cp $2 $3;;
-    ls ) ls $2;; 
-    cat ) cat $2;;
-    unzip ) unzip $2;;
-    bash ) shift; bash $@;;
-    -- ) shift; break ;;
-    "" ) usage ;;
-    * ) usage; exit -1 ;;
+cp) cp $2 $3 ;;
+ls) ls $2 ;;
+cat) cat $2 ;;
+unzip) unzip $2 ;;
+bash)
+    shift
+    bash $@
+    ;;
+--)
+    shift
+    break
+    ;;
+"") usage ;;
+*)
+    usage
+    exit -1
+    ;;
 esac
-
-
-
-
