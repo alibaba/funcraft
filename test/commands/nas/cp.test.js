dd2d4f5a6cf17a92e2e188216392a229d2c8cc4a
git-qfzhang <qianfeng.zqf@alibaba-inc.com>
2019-07-28 14:15:26 +0800

    qinafeng fun nas sync update 20190728 support nasconfig auto

---
+++ test/commands/nas/cp.test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const cp = require('../../../lib/commands/nas/cp');
+
+
+describe('cp local file to NAS test', () => {
+  it('nas path invalid test', () => {
+
+  });
+
+  it('cp file wiht -R option test', () => {
+
+  });
+
+  it('cp folder wihtout -R option test', () => {
+
+  });
+
+  it('cp folder/folder wiht right option test', () => {
+
+  });
+});
+
+describe('cp NAS to NAS', () => {
+  it('nas path invalid', () => {
+
+  });
+
+  it('src service not equal to dst service', () => {
+
+  });
+
+  it('valid cp behavior', () => {
+
+  });
+});
+
+describe('unsupport cp behavior', () => {
+  it('cp NAS to locality', () => {
+
+  });
+
+  it('cp local to locality', () => {
+
+  });
+
+});
