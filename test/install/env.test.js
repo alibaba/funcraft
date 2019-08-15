70a008c391253c70fb16a60186ddcf1a81a648cc
Ellison <sunfeiyu_java@163.com>
2019-08-15 15:52:18 +0800

    test

--- test/install/env.test.js
+++ test/install/env.test.js
@@ -1,7 +1,11 @@
 'use strict';
 
-const { addEnv } = require('../../lib/install/env');
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
 const expect = require('expect.js');
+
+const { addEnv } = require('../../lib/install/env');
 
 describe('install_env', ()=>{
 
@@ -36,4 +40,55 @@ describe('install_env', ()=>{
 
     expect(envs).to.have.property('PYTHONUSERBASE', '/mnt/nas/fun/python');
   });
+});
+
+const fs = {
+  existsSync: sandbox.stub(),
+  readdirSync: sandbox.stub()
+};
+
+const file = {
+  readLines: sandbox.stub()
+};
+
+const envStub = proxyquire('../../lib/install/env', {
+  'fs-extra': fs,
+  '../fc-utils/fc-fun-nas-server/lib/file': file
+});
+
+describe.only('resolveLibPathsFromLdConf', () => {
+
+  afterEach(() => {
+    sandbox.reset();
+  });
+
+  it('resolveLibPathsFromLdConf with path exist', async () => {
+
+    fs.existsSync.returns(true);
+    fs.readdirSync.returns(['a.conf', 'b.conf']);
+    file.readLines.returns(Promise.resolve(['/aaa']));
+
+    const lines = await envStub.resolveLibPathsFromLdConf(process.cwd(), './');
+
+    expect(lines).to.eql({
+      LD_LIBRARY_PATH: '/code/.fun/root/code/.fun/root/aaa:/code/.fun/root/code/.fun/root/aaa'});
+  });
+
+  it('resolveLibPathsFromLdConf with path not exist', async () => {
+
+    fs.existsSync.returns(false);
+    const lines = await envStub.resolveLibPathsFromLdConf(process.cwd(), './');
+    expect(lines).to.eql({});
+  });
+
+  it('resolveLibPathsFromLdConf with front blank for line', async () => {
+
+    fs.existsSync.returns(true);
+    fs.readdirSync.returns(['a.conf', 'b.conf']);
+    file.readLines.returns(Promise.resolve(['      /blank']));
+    const lines = await envStub.resolveLibPathsFromLdConf(process.cwd(), './');
+    expect(lines).to.eql({
+      LD_LIBRARY_PATH: '/code/.fun/root/code/.fun/root/blank:/code/.fun/root/code/.fun/root/blank'});  
+  });
+
 });
