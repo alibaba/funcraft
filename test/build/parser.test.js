ba7a46aac1745413f91c9dcc08a3c196683519e7
tanhe123 <xiayule148@gmail.com>
2019-08-19 09:49:39 +0800

    add parser test

---
+++ test/build/parser.test.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
+const parser = require('../../lib/build/parser');
+const fs = require('fs-extra');
+const expect = require('expect.js');
+
+const assert = sandbox.assert;
+
+const funymlContent = `
+runtime: python3
+tasks:
+  - apt: local-package
+    local: false
+  - apt: libzbar0
+  - shell: ln -sf libzbar.so.0.2.0 libzbar.so
+    cwd: /code/.fun/root/usr/lib
+  - pip: Pillow
+    env: 
+      PATH: testpath
+      TESTENV: testenv
+  - pip: pyzbar
+    local: true
+  - name: compile R
+    shell: |-
+      apt-get build-dep -y r-base;
+      curl -L https://fc-demo-public.oss-cn-hangzhou.aliyuncs.com/fun/examples/R-3.5.2.tar.gz | tar -zxf -;
+      cd R-3.5.2 ; 
+      ./configure --prefix=/code/.fun/R/ --enable-R-shlib --with-blas --with-lapack ; 
+      make ; 
+      make install;
+    env: 
+      LD_LIBRARY: testLD_LIBRARY
+`;
+
+const funfileContent = `RUNTIME python3
+COPY . /code
+WORKDIR /code
+RUN apt-get install local-package
+RUN fun-install apt-get install libzbar0
+RUN cd /code/.fun/root/usr/lib && ln -sf libzbar.so.0.2.0 libzbar.so
+RUN PATH=testpath:$PATH TESTENV=testenv fun-install pip install Pillow
+RUN fun-install pip install pyzbar
+RUN LD_LIBRARY=testLD_LIBRARY apt-get build-dep -y r-base; \\
+  LD_LIBRARY=testLD_LIBRARY curl -L https://fc-demo-public.oss-cn-hangzhou.aliyuncs.com/fun/examples/R-3.5.2.tar.gz | tar -zxf -; \\
+  LD_LIBRARY=testLD_LIBRARY cd R-3.5.2 ;  \\
+  LD_LIBRARY=testLD_LIBRARY ./configure --prefix=/code/.fun/R/ --enable-R-shlib --with-blas --with-lapack ;  \\
+  LD_LIBRARY=testLD_LIBRARY make ;  \\
+  LD_LIBRARY=testLD_LIBRARY make install;`;
+
+const dockerfileContent = `FROM aliyunfc/runtime-python3.6:build-1.5.7
+COPY . /code
+WORKDIR /code
+RUN apt-get install local-package
+RUN fun-install apt-get install libzbar0
+RUN cd /code/.fun/root/usr/lib && ln -sf libzbar.so.0.2.0 libzbar.so
+RUN PATH=testpath:$PATH TESTENV=testenv fun-install pip install Pillow
+RUN fun-install pip install pyzbar
+RUN LD_LIBRARY=testLD_LIBRARY apt-get build-dep -y r-base; \\
+  LD_LIBRARY=testLD_LIBRARY curl -L https://fc-demo-public.oss-cn-hangzhou.aliyuncs.com/fun/examples/R-3.5.2.tar.gz | tar -zxf -; \\
+  LD_LIBRARY=testLD_LIBRARY cd R-3.5.2 ;  \\
+  LD_LIBRARY=testLD_LIBRARY ./configure --prefix=/code/.fun/R/ --enable-R-shlib --with-blas --with-lapack ;  \\
+  LD_LIBRARY=testLD_LIBRARY make ;  \\
+  LD_LIBRARY=testLD_LIBRARY make install;`;
+
+
+describe('test funymlToFunfile', () => {
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('test funymlToFunfile', async function () {
+    sandbox.stub(fs, 'readFileSync').returns(funymlContent);
+
+    const funFile = await parser.funymlToFunfile('path');
+    expect(funFile).to.equal(funfileContent);
+  });
+});
+
+describe('test funfileToDockerfile', () => {
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('test funfileToDockerfile', async function () {
+    sandbox.stub(fs, 'readFile').returns(funfileContent);
+
+    const dockerfile = await parser.funfileToDockerfile('path');
+    expect(dockerfile).to.equal(dockerfileContent);
+  });
+});
