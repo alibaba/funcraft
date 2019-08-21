ca894394a83d96dc0b5ca3e6262727ded583a6f5
倚贤 <yixian.dw@alibaba-inc.com>
2019-08-21 14:06:32 +0800

    fix test

--- test/bin/fun-install.test.js
+++ test/bin/fun-install.test.js
@@ -7,7 +7,7 @@ describe('sbox', function() {
 
   it('tty=true, interactive=true', (done) => {
     new ExpectedShell()
-      .when('1$').respond('bin/fun.js install sbox --runtime nodejs8 --interactive\r')
+      .when('1$').respond('node bin/fun.js install sbox --runtime nodejs8 --interactive\r')
       .when('root@fc-nodejs8:/code#  \b \b').respond('exit\r')
       .when('2$').respond('exit\r')
       .end(done);
@@ -15,21 +15,21 @@ describe('sbox', function() {
 
   it('tty=false, interactive=false', (done) => {
     new ExpectedShell()
-      .when('1$').respond('bin/fun.js install sbox --runtime nodejs8 --cmd \'echo hello\'\r')
+      .when('1$').respond('node bin/fun.js install sbox --runtime nodejs8 --cmd \'echo hello\'\r')
       .when('hello\r\n2$').respond('exit\r')
       .end(done);
   });
 
   it('tty=false, interactive=true', (done) => {
     new ExpectedShell()
-      .when('1$').respond('echo hello | bin/fun.js install sbox --runtime nodejs8 --interactive --cmd \'cat -\'\r')
+      .when('1$').respond('echo hello | node bin/fun.js install sbox --runtime nodejs8 --interactive --cmd \'cat -\'\r')
       .when('hello\r\n2$').respond('exit\r')
       .end(done);
   });
 
   it('env', (done) => {
     new ExpectedShell()
-      .when('1$').respond('bin/fun.js install sbox --runtime nodejs8 --env A=b --cmd \'bash -c "env | grep A="\'\r')
+      .when('1$').respond('node bin/fun.js install sbox --runtime nodejs8 --env A=b --cmd \'bash -c "env | grep A="\'\r')
       .when('A=b\r\n2$').respond('exit\r')
       .end(done);
   });
