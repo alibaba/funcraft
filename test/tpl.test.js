a4e1c984367e89ed692c7494033a56874f9b5232
tanhe123 <xiayule148@gmail.com>
2019-10-11 15:39:47 +0800

    fix tmpDir path when using fun build

--- test/tpl.test.js
+++ test/tpl.test.js
@@ -26,3 +26,23 @@ describe('test detectNasBaseDir', () => {
     expect(tpl.detectNasBaseDir(path.join(process.cwd(), tpl.DEFAULT_BUILD_ARTIFACTS_PATH_SUFFIX, 'template.yml'))).to.eql(path.join(process.cwd(), tpl.DEFAULT_NAS_PATH_SUFFIX));
   });
 });
+
+describe('test detectTmpDir', () => {
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('test normal', () => {
+    const pathExistsStub = sandbox.stub(fs, 'pathExists');
+    pathExistsStub.withArgs(path.join(process.cwd(), tpl.DEFAULT_BUILD_ARTIFACTS_PATH_SUFFIX, 'template.yml')).resolves(false);
+    pathExistsStub.withArgs(path.join(process.cwd(), tpl.DEFAULT_BUILD_ARTIFACTS_PATH_SUFFIX, 'template.yaml')).resolves(false);
+    pathExistsStub.withArgs(path.join(process.cwd(), 'template.yml')).resolves(true);
+
+    expect(tpl.detectTmpDir(path.join('template.yml'))).to.eql(path.join(process.cwd(), tpl.DEFAULT_TMP_INVOKE_PATH_SUFFIX));
+  });
+
+  it('test built template yml', () => {
+    sandbox.stub(fs, 'pathExists').withArgs(path.join(tpl.DEFAULT_BUILD_ARTIFACTS_PATH_SUFFIX, 'template.yml')).resolves(true);
+    expect(tpl.detectTmpDir(path.join(process.cwd(), tpl.DEFAULT_BUILD_ARTIFACTS_PATH_SUFFIX, 'template.yml'))).to.eql(path.join(process.cwd(), tpl.DEFAULT_TMP_INVOKE_PATH_SUFFIX));
+  });
+});
