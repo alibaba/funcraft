878bd1e86b323e7361c8c40f4822bcaca53a915c
coco-super <sunfeiyu_java@163.com>
2019-10-12 13:03:11 +0800

    test

--- test/ram.test.js
+++ test/ram.test.js
@@ -2,7 +2,27 @@
 
 const { normalizeRoleOrPoliceName } = require('../lib/ram');
 
+const sinon = require('sinon');
+const sandbox = sinon.createSandbox();
+const assert = sinon.assert;
+const ram = require('../lib/ram');
+const Ram = require('@alicloud/ram');
 const expect = require('expect.js');
+
+const policy = {
+  'Policies': {
+    'Policy': [
+      {
+        'Description': '管理对象存储服务(OSS)权限',
+        'PolicyName': 'AliyunOSSFullAccess',
+        'AttachDate': '2019-10-12T02:09:55Z',
+        'DefaultVersion': 'v1',
+        'PolicyType': 'System'
+      }
+    ]
+  },
+  'RequestId': '762790B0-1D91-473A-A8C1-F6EF7B44427A'
+};
 
 describe('test normalizeRoleOrPoliceName', () => {
   it('test valid', () => {
@@ -15,5 +35,38 @@ describe('test normalizeRoleOrPoliceName', () => {
     const roleName = normalizeRoleOrPoliceName('test_role_name');
     
     expect(roleName).to.be('test-role-name');
+  });
+});
+
+describe('ram police capitalization test', () => {
+
+  beforeEach(() => {
+
+    sandbox.stub(Ram.prototype, 'listPoliciesForRole').resolves(policy);
+    sandbox.stub(Ram.prototype, 'attachPolicyToRole');
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('ram police lower case', async () => {
+    const policyName = 'aliyunossfullaccess';
+    await ram.attachPolicyToRole(policyName, 'roleName');
+
+    assert.calledWith(Ram.prototype.listPoliciesForRole, {
+      RoleName: 'roleName'
+    });
+    assert.notCalled(Ram.prototype.attachPolicyToRole);
+  });
+
+  it('ram police capital case', async () => {
+    const policyName = 'ALIYUNOSSFULLACCESS';
+    await ram.attachPolicyToRole(policyName, 'roleName');
+
+    assert.calledWith(Ram.prototype.listPoliciesForRole, {
+      RoleName: 'roleName'
+    });
+    assert.notCalled(Ram.prototype.attachPolicyToRole);
   });
 });
