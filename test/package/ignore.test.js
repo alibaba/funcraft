a74dc8012f61f35b0eb8bbed095feb282f9650ab
coco-super <sunfeiyu_java@163.com>
2019-11-13 17:16:58 +0800

    检测 fun deploy 时打包的代码超过限制（50M） fun 提示自动化部署 nas auto 方案。 检测语言依赖及系统依赖添加挂载信息到 .fun/nasMappings.json 中。 fun nas sync 时 如果存在 nasMappings.json 文件，则添加到 service nas mapping 中

--- test/package/ignore.test.js
+++ test/package/ignore.test.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const path = require('path'), 
+const path = require('path'),
   expect = require('expect.js'),
   ignore = require('../../lib/package/ignore');
 
-describe('funignore', () => {
+const fs = require('fs-extra');
 
-  it('default ignore', () => {
-    var funignore = ignore(__dirname);
-    
+describe('funignore', async () => {
+
+  it('default ignore', async () => {
+    var funignore = await ignore(__dirname);
+
     expect(funignore(path.join(__dirname, '.env'))).to.be.ok();
     expect(funignore(path.join(__dirname, '.svn'))).to.be.ok();
     expect(funignore(path.join(__dirname, '.git'))).to.be.ok();
@@ -18,8 +20,8 @@ describe('funignore', () => {
 
   });
 
-  it('from .funignore', () => {
-    var funignore = ignore(__dirname);
+  it('from .funignore', async () => {
+    var funignore = await ignore(__dirname);
 
     expect(funignore(path.join(__dirname, '1.log'))).to.be.ok();
     expect(funignore(path.join(__dirname, '1.log1'))).not.to.be.ok();
@@ -29,17 +31,47 @@ describe('funignore', () => {
 
   });
 
-  it('empty path', () => {
-    var funignore = ignore(__dirname);
+  it('empty path', async () => {
+    var funignore = await ignore(__dirname);
 
     expect(funignore(__dirname)).not.to.be.ok();
   });
 
-  it('unignore env', () => {
+  it('unignore env', async () => {
     var baseDir = path.join(__dirname, 'unignore');
-    var funignore = ignore(baseDir);
+    var funignore = await ignore(baseDir);
 
     expect(funignore(path.join(baseDir, '.env'))).not.to.be.ok();
   });
 
+  it('test nasMappings.json', async () => {
+    const baseDir = process.cwd();
+    const nasMappingsPath = path.resolve(baseDir, '.fun', 'nasMappings.json');
+
+    await fs.ensureFile(nasMappingsPath);
+
+    const content = {
+      'MyService': [
+        {
+          'localNasDir': '/Users/ellison/fun/.fun/root',
+          'remoteNasDir': '/mnt/auto/root'
+        },
+        {
+          'localNasDir': '/Users/ellison/fun/node_modules',
+          'remoteNasDir': '/mnt/auto/node_modules'
+        }
+      ]
+    };
+
+    await fs.outputFile(nasMappingsPath, JSON.stringify(content, null, 4));
+
+    var funignore = await ignore(baseDir);
+
+    expect(funignore(path.join(baseDir, 'fun', 'nasMappings.json'))).to.be.ok();
+    expect(funignore(path.join(baseDir, 'fun', '1.log'))).not.to.be.ok();
+    expect(funignore(path.join(baseDir, 'node_modules'))).to.be.ok();
+    expect(funignore(path.join(baseDir, '.fun/root'))).to.be.ok();
+
+    await fs.remove(nasMappingsPath)
+  });
 });
