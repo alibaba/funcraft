f1ed3b6426b5b961cfbb17a09aa5b8c7e6c6d919
Ellison <sunfeiyu_java@163.com>
2020-01-06 21:26:37 +0800

    ros auto configuration for fun package (#716)

--- test/package/package.test.js
+++ test/package/package.test.js
@@ -16,17 +16,23 @@ describe('test package', () => {
   const tplPath = 'tplPath';
   const bucket = 'bucket';
   const outputTemplateFile = 'outputTemplateFile';
-  const updatedTpl = 'updatedTpl';
-  const tplContent = 'tplContent';
+
+  const mock = require('../tpl-mock-data');
 
   let ossClient;
-  
+
   beforeEach(() => {
     ossClient = sandbox.stub();
 
-    sandbox.stub(tpl, 'getTpl').resolves(tplContent); 
+    sandbox.stub(tpl, 'getTpl').resolves(mock.tpl);
     sandbox.stub(client, 'getOssClient').resolves(ossClient);
-    sandbox.stub(template, 'uploadAndUpdateFunctionCode').resolves(updatedTpl);
+    sandbox.stub(template, 'uploadAndUpdateFunctionCode').resolves(mock.tpl);
+    sandbox.stub(template, 'zipToOss').resolves('objectName');
+    sandbox.stub(template, 'uploadNasService').resolves('./');
+    sandbox.stub(template, 'generateRosTemplateForNasService').returns({});
+    sandbox.stub(template, 'generateRosTemplateForResources').returns({});
+    sandbox.stub(template, 'generateRosTemplateForOutputs').returns({});
+    sandbox.stub(template, 'generateRosTemplateForRegionMap').returns({});
     sandbox.stub(util, 'outputTemplateFile').returns();
 
     pack = proxyquire('../../lib/package/package', {
@@ -47,8 +53,8 @@ describe('test package', () => {
 
     assert.calledWith(tpl.getTpl, tplPath);
     assert.calledWith(client.getOssClient, bucket);
-    assert.calledWith(template.uploadAndUpdateFunctionCode, path.dirname(tplPath), tplContent, ossClient);
-    assert.calledWith(util.outputTemplateFile, packedYmlPath, updatedTpl);
+    assert.calledWith(template.uploadAndUpdateFunctionCode, path.dirname(tplPath), mock.tpl, ossClient);
+    assert.calledWith(util.outputTemplateFile, packedYmlPath, mock.tpl);
   });
 
   it('test default outputTemplateFile', async () => {
@@ -58,7 +64,7 @@ describe('test package', () => {
 
     assert.calledWith(tpl.getTpl, tplPath);
     assert.calledWith(client.getOssClient, bucket);
-    assert.calledWith(template.uploadAndUpdateFunctionCode, path.dirname(tplPath), tplContent, ossClient);
-    assert.calledWith(util.outputTemplateFile, packedYmlPath, updatedTpl);
+    assert.calledWith(template.uploadAndUpdateFunctionCode, path.dirname(tplPath), mock.tpl, ossClient);
+    assert.calledWith(util.outputTemplateFile, packedYmlPath, mock.tpl);
   });
 });
