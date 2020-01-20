8bccf4c7c33da4bf6212f1eccd0795728ccd4bd6
coco-super <sunfeiyu_java@163.com>
2020-01-20 10:44:09 +0800

    test

--- test/build/build.test.js
+++ test/build/build.test.js
@@ -21,7 +21,6 @@ const os = require('os');
 const path = require('path');
 const yaml = require('js-yaml');
 const { red } = require('colors');
-const file = require('../../lib/utils/file');
 const _ = require('lodash');
 
 const { DEFAULT_NAS_PATH_SUFFIX, DEFAULT_BUILD_ARTIFACTS_PATH_SUFFIX } = require('../../lib/tpl');
@@ -51,9 +50,6 @@ describe('test buildFunction', () => {
 
   beforeEach(() => {
     sandbox.stub(fs, 'writeFile');
-    sandbox.stub(fs, 'createReadStream');
-    sandbox.stub(file, 'readLines').resolves([]);
-
     pathExistsStub = sandbox.stub(fs, 'pathExists');
 
     pathExistsStub.withArgs(path.resolve(baseDir)).resolves(true);
