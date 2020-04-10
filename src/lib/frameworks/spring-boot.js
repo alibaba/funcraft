293d5a036ba0eb47b88e5dc7f4d6153938110116
Hehe Tan <xiayule148@gmail.com>
2020-04-10 17:39:09 +0800

    add single spring boot jar support (#859)

--- src/lib/frameworks/spring-boot.js
+++ src/lib/frameworks/spring-boot.js
@@ -3,21 +3,64 @@
 const path = require('path');
 const fs = require('fs-extra');
 const _ = require('lodash');
+const file = require('./common/file');
+const { isSpringBootJar } = require('./common/spring-boot');
 
 const springboot = {
   'id': 'springboot',
   'runtime': 'java',
   'website': 'https://spring.io/projects/spring-boot/',
   'detectors': {
-    'and': [
+    'or': [
       {
         'type': 'regex',
         'path': 'pom.xml',
         'content': '<artifactId>\\s*spring-boot-starter-parent\\s*</artifactId>'
+      },
+      {
+        'type': 'file',
+        'path': /\.jar$/
       }
     ]
   },
   'actions': [
+    {
+      'condition': {
+        'and': [
+          {
+            'type': 'file',
+            'path': /\.jar$/
+          }
+        ]
+      },
+      'processors': [
+        {
+          'type': 'function',
+          'function': async (codeDir) => {
+            const jars = await file.listDir(codeDir, /\.jar$/);
+
+            if (jars.length > 1) {
+              throw new Error('We detected you have more than 1 jar in current folder.');
+            }
+            
+            const jar = jars[0];
+            
+            if (!await isSpringBootJar(jar)) {
+              throw new Error('Only Spring Boot jar is supported');
+            }
+
+            const bootstrap = `#!/usr/bin/env bash
+export PORT=9000
+java -jar -Dserver.port=$PORT ${path.relative(codeDir, jar)}
+`;
+            
+            await fs.writeFile('bootstrap', bootstrap, {
+              mode: '0755'
+            });
+          }
+        }
+      ]
+    },
     {
       'condition': true,
       'description': 'find jar under target/ and generate bootstrap',
@@ -42,7 +85,7 @@ You can use 'mvn package' to package SpringBoot to a jar.`);
                 jarFiles.push(relative);
               }
             }
-            
+
             if (jarFiles.length === 0) {
               throw new Error(`Could not find any jar from 'target' folder.
 You can use 'mvn package' to package SpringBoot to a jar.`);
