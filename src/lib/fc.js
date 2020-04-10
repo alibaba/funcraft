f20e5d3728430b57b557491f99166cca410a132f
coco-super <sunfeiyu_java@163.com>
2020-04-10 16:33:29 +0800

    fix

--- src/lib/fc.js
+++ src/lib/fc.js
@@ -1019,7 +1019,9 @@ async function generateFontsConfAndEnv(baseDir, codeUri, appendContet) {
 
   if (!await fs.pathExists(fontsConfPath)) {
     const sourcePath = path.resolve(__dirname, './utils/fonts/fonts.conf');
-    await fs.copyFile(sourcePath, fontsConfPath);
+    // 无论是 win，mac 还是 linux 平台，运行 pkg 生成的二进制可执行文件时会其解压到 snapshot 目录 (windows 中的C:\snapshot\)
+    // 在使用 fs-extra 的 copyFile 方法时，会提示 /snapshot/fun/lib/utils/fonts/fonts.conf, 推测 pkg 只对原生的 fs 支持，不兼容 fs-extra 中额外实现的方法。
+    await fs.writeFile(fontsConfPath, await fs.readFile(sourcePath, 'utf8'));
   }
 
   if (appendContet) {
