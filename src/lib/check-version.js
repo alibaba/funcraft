bd9bd4c978d2c57125c78ab17cf2dc98e86a28c8
coco-super <sunfeiyu_java@163.com>
2020-04-21 15:34:23 +0800

    posinstallt

--- src/lib/check-version.js
+++ src/lib/check-version.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var tryRequire = require('try-require');
-var pkg = tryRequire('../../package') || tryRequire('../package');
-var semver = require('semver');
+const tryRequire = require('try-require');
+const pkg = tryRequire('../../package') || tryRequire('../package');
+const semver = require('semver');
 
-var version = pkg.engines.node;
+const version = pkg.engines.node;
 if (!semver.satisfies(process.version, version)) {
   throw new Error(`\x1b[31m@alicloud/fun required node version ${version} not satisfied with current version ${process.version}.\x1b[0m\n`);
 }
