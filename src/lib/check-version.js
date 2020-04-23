0a0575253f95cd02bc3873e0a6dcfa4876d38190
coco-super <sunfeiyu_java@163.com>
2020-04-23 21:10:31 +0800

    finish

--- src/lib/check-version.js
+++ src/lib/check-version.js
@@ -2,13 +2,9 @@
 
 const path = require('path');
 const tryRequire = require('try-require');
-
-console.log(path.join(__dirname, '../package'));
-console.log(path.join(__dirname, '../../package'));
-
 const pkg = tryRequire(path.join(__dirname, '../package')) || tryRequire(path.join(__dirname, '../../package'));
-console.log(pkg);
 const semver = require('semver');
+
 const version = pkg.engines.node;
 if (!semver.satisfies(process.version, version)) {
   throw new Error(`\x1b[31m@alicloud/fun required node version ${version} not satisfied with current version ${process.version}.\x1b[0m\n`);
