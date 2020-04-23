bc0a19315704c872179d3b25abde6e194e15ff9a
coco-super <sunfeiyu_java@163.com>
2020-04-23 22:03:45 +0800

    trivals again

--- src/lib/deploy/deploy-by-tpl.js
+++ src/lib/deploy/deploy-by-tpl.js
@@ -499,7 +499,6 @@ async function deployCustomDomain(domainName, domainDefinition, routes) {
   if (_.isEmpty(certConfig) && protocol === 'HTTP,HTTPS') {
     throw new Error(red(`\nMust config "CertConfig" for CustomDomain "${domainName}" when using "HTTP,HTTPS" protocol.\nYou can refer to https://github.com/aliyun/fun/blob/master/docs/specs/2018-04-03-zh-cn.md#aliyunserverlesscustomdomain\nor https://github.com/aliyun/fun/blob/master/docs/specs/2018-04-03.md/#aliyunserverlesscustomdomain for help.`));
   }
-
   if (!_.isEmpty(certConfig) && protocol === 'HTTP') {
     throw new Error(red(`\nPlease don't use "CertConfig" config of CustomDomain "${domainName}" when using "HTTP" protocol.\nYou can refer to https://github.com/aliyun/fun/blob/master/docs/specs/2018-04-03-zh-cn.md#aliyunserverlesscustomdomain\nor https://github.com/aliyun/fun/blob/master/docs/specs/2018-04-03.md/#aliyunserverlesscustomdomain for help.`));
   }
