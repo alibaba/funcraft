8a78cc3dba7524ba8887afb156906e0666772a44
coco-super <sunfeiyu_java@163.com>
2020-04-24 21:37:46 +0800

    fix name

--- src/lib/vswitch.js
+++ src/lib/vswitch.js
@@ -151,20 +151,20 @@ async function describeVpcZones(vpcClient, region) {
 async function convertToFcAllowedZones(vpcClient, region, vswitchIds) {
   const fcAllowedZones = await getFcAllowedZones();
 
-  const zoneObj = [];
+  const fcZones = [];
   for (const vswitchId of vswitchIds) {
     const zoneId = await getVSwitchZoneId(vpcClient, region, vswitchId);
     if (_.includes(fcAllowedZones, zoneId)) {
-      zoneObj.push({ zoneId, vswitchId });
+      fcZones.push({ zoneId, vswitchId });
     }
   }
-  if (_.isEmpty(zoneObj)) {
+  if (_.isEmpty(fcZones)) {
     throw new Error(`
 Only zoneId ${fcAllowedZones} of vswitch is allowed by VpcConfig.
 Check your vswitch zoneId please.`);
   }
 
-  return zoneObj;
+  return fcZones;
 }
 
 function convertZones(nasZones, zones, storageType = 'Performance') {
