90a6c1b167d1022433d2c29d1e2a149d97e78297
wss-git <wssgryx@163.com>
2020-11-09 15:32:08 +0800

    fix(deploy/parameter-override): fun deploy --use-ros -p parsing failed
    fix #1030

--- src/lib/build/parser.js
+++ src/lib/build/parser.js
@@ -169,10 +169,10 @@ function parsePairs(val, vars) {
    * keys can only contain letters, numbers, and underscores
    * values can be any character
    */
-  const group = val.match(/(^[a-zA-Z_][a-zA-Z\d_]*)=(.*)/);
+  const group = val.match(/(^[a-zA-Z_][a-zA-Z\d_]*)=.*/);
   vars = vars || {};
   if (group) {
-    vars[group[1]] = group[2];
+    vars[group[1]] = val.substring(val.indexOf('=') + 1); // fix: https://github.com/alibaba/funcraft/issues/1030
   }
   return vars;
 }
